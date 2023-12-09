a3c6c73e4a88786d570891f6cda4ddbd8032ef8f
damiv778 <damianivangaston@gmail.com>
2023-12-09 18:41:21 -0300

    arreglo resistro

--- JS/login.js
+++ JS/login.js
@@ -1,46 +1,14 @@
-
-let usuario = document.getElementById('username').value;
-let password = document.getElementById('password').value;
-
-console.log(usuario);
-console.log(password);
-
-
-let administrador = {
-    usuario: '38778715',
-    password: '456456'
-}
-
-console.log(`El usuario es ${administrador.usuario} y la contraseña es ${administrador.password}`);
-
-
-//Funcion Error
-function error() {
-    console.log('Su Usuario o Contraseña es INCORRECTO');
-    alert('Su Usuario o Contraseña es INCORRECTO');
-}
-
-function login() {
-
-    let user = document.getElementById('username').value;
-    let password = document.getElementById('password').value;
-    
-    console.log(`El usuario es ${usuario} y la contraseña es ${password}`);
-
-    if(username == "" || password == ""){
-        //alert('Por favor ingrese un usuario y contraseña');
-        location.href = 'error.html'
+const loginForm = document.querySelector("#loginForm")
+loginForm.addEventListener("submit", (e)=>{
+    e.preventDefault()
+    const email = document.querySelector("#email").value
+    const password = document.querySelector("#password").value
+    const Users = JSON.parse(localStorage.getItem("users")) || []
+    const validUser = Users.find(user => user.email === email && user.password === password)
+    if (!validUser){
+        return alert("Usuario o contraseña incorrectos")
     }
+    alert("¡Bienvenido a Escalera Educativa!")
+    window.location.href = "../login.html"
 
-    if(administrador.username == usuario && administrador.password == password){
-       location.href = 'index.html'
-    }else{
-       console.log(location.pathname)
-    }
-
-    let inputUser = document.getElementById('usuario');
-    inputUser.value = "";
-
-    let inputPass = document.getElementById('password');
-    inputPass.value = ""
-}
+})
